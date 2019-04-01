8a189f522e68a3ab1405bba7b90c28a876f79f1f
github username <github email address>
2019-04-01 01:14:49 -0400

    enabled sessions hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret_wanderlust"
   end
 
   get "/" do
