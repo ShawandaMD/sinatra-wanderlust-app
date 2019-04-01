6714d701787868d6634dce98b9d719bb3c1be331
github username <github email address>
2019-04-01 12:28:10 -0400

    added has_secure_password method to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :journal_entries
-  has_many :countries, through: :journal_entries 
+  has_many :countries, through: :journal_entries
 end
