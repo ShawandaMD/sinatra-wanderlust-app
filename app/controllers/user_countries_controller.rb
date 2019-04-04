f85a5f4be4e2ad3812a9c9fa1b4da75c41e0680e
github username <github email address>
2019-04-04 12:46:14 -0400

    commited out '/welcome/:id/:name' route

--- app/controllers/user_countries_controller.rb
+++ app/controllers/user_countries_controller.rb
@@ -1,12 +1,12 @@
 class UserCountriesController < ApplicationController
 
-  get '/welcome/:id/:name' do
-    if logged_in?
-      @user = current_user
-      erb :"/countries/welcome"
-    else
-      redirect to "/"
-    end
-  end
+  #get '/welcome/:id/:name' do
+  #  if logged_in?
+  #    @user = current_user
+  #    erb :"/countries/welcome"
+  #  else
+  #    redirect to "/"
+  #  end
+  #end
 
 end
