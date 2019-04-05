079cae2ad25fa69034a400b74d125eb0a3e7b410
github username <github email address>
2019-04-04 21:26:05 -0400

    added helper method check if session user_id matches JournalEntry user_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,15 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do #written in controller and accessible in views
+    #write one for making sure the user id and session id macthes
     def logged_in? #will return true or false
       !!session[:user_id]
     end
     def current_user #finds the current user
       User.find(session[:user_id])
+    end
+    def belongs_to_user_id
+      !!User.find(session[:user_id]) == JournalEntry.user_id
     end
   end
 
