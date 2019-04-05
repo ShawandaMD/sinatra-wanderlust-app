dcbcb3d7581b799f08af408cc677b212f1934940
github username <github email address>
2019-04-05 09:20:55 -0400

    added validation methods to check for presence and uniqueness of attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :journal_entries
   has_many :countries, through: :journal_entries
+  validates_presence_of :name, :email, :password
+  validates_uniqueness_of :email
 end
