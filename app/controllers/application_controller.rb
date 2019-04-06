0b649ae5830d37f2623e31dc5576991b73dc7e69
github username <github email address>
2019-04-06 12:24:18 -0400

    added rack-flash to the configure block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
-require './config/environment'
+require 'sinatra/base'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,13 +8,15 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret_wanderlust"
+    use Rack::Flash
   end
+
 
   get "/" do
     erb :index
   end
 
-  helpers do 
+  helpers do
     def logged_in? #will return true or false
       !!session[:user_id]
     end
